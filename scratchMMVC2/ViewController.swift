6aba92926abe3b4d4a724ff78f18bcc52efe7764
Vince O'Sullivan (from laptop) <vjosullivan@gmail.com>
2017-11-11 20:32:06 +0000

    Initial Commit

---
+++ scratchMMVC2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scratchMMVC2
+//
+//  Created by Vincent O'Sullivan on 11/11/2017.
+//  Copyright © 2017 Vincent O'Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
